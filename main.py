97667ca22033fff4530c2ddc010dd0b2df54e111
AzuDR <dominguez.azucena@uabc.edu.mx>
2023-12-03 12:30:09 -0800

    listo

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from catalogoGP import archivo
 from Procesamiento_datos import limpieza
 from CatalogoProcesado import redondear
 from duplicadosProyecto import depuracion
+from sqlalchemy import create_engine
+import CONSTANTES as cs
 
 data = archivo()
 data = pd.DataFrame(data)
@@ -11,6 +13,19 @@ data = redondear(data)
 data = depuracion(data) #escoja el metodo bfill o ffill para rellenar los datos faltantes
 
 data.to_csv("Catalogo_Terminado.csv")
-print("Terminé")
-
 #print(data.dtypes)
+
+#CONEXION MYSQL
+
+cadena_conexion = f"mysql+mysqlconnector://{cs.USER}:{cs.PASSWORD}@{cs.SERVER}/{cs.NAME_BD}"
+#print(cadena_conexion)
+engine = create_engine(cadena_conexion)
+conexion = engine.connect()
+#print(conexion)
+datosdata = pd.read_csv("Catalogo_Terminado.csv", index_col=0)
+datosdata.to_sql("juegos", conexion, if_exists="append", index=False)
+print("Se hara la siguiente consulta para asegurar que se hizo la conexion:\n\n")
+query = "Select * from juegos where Año= 2017 "
+resultados = pd.read_sql(query, conexion)
+print(resultados)
+print("La primera parte del proyecto ha TERMINADO")
